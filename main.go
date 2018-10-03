81f7c71c481f717d78cdc3a9026056cc2b52d59e
Robbie McMichael <2044464+robbiemcmichael@users.noreply.github.com>
2018-10-03 18:38:36 +1000

    Map claims between tokens

--- main.go
+++ main.go
@@ -19,15 +19,22 @@ type Config struct {
 	Issuers    []*Issuer `json:"issuers"`
 }
 
-type Secrets struct {
-	Algorithm  string `json:"algorithm"`
-	PrivateKey string `json:"privateKey"`
-}
-
 type Issuer struct {
 	Issuer          string      `json:"issuer"`
 	PublicKey       string      `json:"publicKey"`
 	ParsedPublicKey interface{} // *rsa.PublicKey, *dsa.PublicKey or *ecdsa.PublicKey
+	Mappings        []Mapping   `json:"mappings"`
+}
+
+type Mapping struct {
+	From   string `json:"from"`
+	To     string `json:"to"`
+	Prefix string `json:"prefix"`
+}
+
+type Secrets struct {
+	Algorithm  string `json:"algorithm"`
+	PrivateKey string `json:"privateKey"`
 }
 
 func main() {
@@ -72,13 +79,6 @@ func mainE() error {
 		return err
 	}
 
-	signedToken, err := config.issueToken()
-	if err != nil {
-		return fmt.Errorf("failed to sign token: %s", err)
-	}
-
-	fmt.Printf("Signed token: %s\n", signedToken)
-
 	for _, issuer := range config.Issuers {
 		err := issuer.parsePublicKey()
 		if err != nil {
@@ -86,17 +86,19 @@ func mainE() error {
 		}
 	}
 
-	for _, issuer := range config.Issuers {
-		claims, err := issuer.getClaims(config.Token)
-		if err != nil {
-			fmt.Printf("failed to get claims: %s\n", err)
-		} else {
-			fmt.Printf("Claims: %+v\n", claims)
-			return nil
-		}
+	newClaims, err := config.validateToken(config.Token)
+	if err != nil {
+		return err
 	}
 
-	return fmt.Errorf("failed to validate token")
+	signedToken, err := config.issueToken(newClaims)
+	if err != nil {
+		return fmt.Errorf("failed to sign token: %s", err)
+	}
+
+	fmt.Printf("Signed token: %s\n", signedToken)
+
+	return nil
 }
 
 func (config *Config) loadSigningKey(secrets *Secrets) error {
@@ -125,13 +127,43 @@ func (config *Config) loadSigningKey(secrets *Secrets) error {
 	return nil
 }
 
-func (config *Config) issueToken() (string, error) {
+func (config *Config) validateToken(token string) (map[string]interface{}, error) {
+	expectedIssuer, err := getIssuer(token)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, issuer := range config.Issuers {
+		if issuer.Issuer == expectedIssuer {
+			claims, err := issuer.getClaims(token)
+			if err != nil {
+				return nil, fmt.Errorf("validation for issuer '%s' failed: %s", expectedIssuer, err)
+			} else {
+				fmt.Printf("Validated claims: %+v\n", claims)
+
+				newClaims, err := issuer.mapClaims(claims)
+				if err != nil {
+					return nil, err
+				}
+
+				fmt.Printf("Original claims: %+v\n", claims)
+				fmt.Printf("Mapped claims: %+v\n", newClaims)
+
+				return newClaims, nil
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("%s is not a valid issuer", expectedIssuer)
+}
+
+func (config *Config) issueToken(claims map[string]interface{}) (string, error) {
 	signer, err := jose.NewSigner(config.SigningKey, &jose.SignerOptions{})
 	if err != nil {
 		return "", fmt.Errorf("failed to get signer: %s", err)
 	}
 
-	payload, err := json.Marshal(config.Claims)
+	payload, err := json.Marshal(claims)
 	if err != nil {
 		return "", fmt.Errorf("failed to marshal claims: %s", err)
 	}
@@ -157,6 +189,20 @@ func (issuer *Issuer) parsePublicKey() error {
 
 	issuer.ParsedPublicKey = key
 	return nil
+}
+
+func getIssuer(tokenString string) (string, error) {
+	token, err := jwt.ParseSigned(tokenString)
+	if err != nil {
+		return "", err
+	}
+
+	var publicClaims jwt.Claims
+	if err := token.UnsafeClaimsWithoutVerification(&publicClaims); err != nil {
+		return "", fmt.Errorf("failed to get public claims: %s", err)
+	}
+
+	return publicClaims.Issuer, nil
 }
 
 func (issuer *Issuer) getClaims(tokenString string) (map[string]interface{}, error) {
@@ -193,4 +239,41 @@ func (issuer *Issuer) getClaims(tokenString string) (map[string]interface{}, err
 	}
 
 	return claimsMap, nil
+}
+
+func (issuer *Issuer) mapClaims(src map[string]interface{}) (map[string]interface{}, error) {
+	dest := make(map[string]interface{})
+	for _, mapping := range issuer.Mappings {
+		if mapping.Prefix != "" {
+			claim, err := prefixClaim(mapping.Prefix, src[mapping.From])
+			if err != nil {
+				return nil, err
+			}
+			dest[mapping.To] = claim
+		} else {
+			dest[mapping.To] = src[mapping.From]
+		}
+	}
+
+	return dest, nil
+}
+
+func prefixClaim(prefix string, claim interface{}) (interface{}, error) {
+	switch claim.(type) {
+	case string:
+		return prefix + claim.(string), nil
+	case []interface{}:
+		array := []string{}
+		for _, element := range claim.([]interface{}) {
+			elementString, ok := element.(string)
+			if ok {
+				array = append(array, prefix+elementString)
+			} else {
+				return nil, fmt.Errorf("claim must be array of strings (%+v)\n", claim)
+			}
+		}
+		return array, nil
+	default:
+		return nil, fmt.Errorf("claim must be string or array of strings (%+v)\n", claim)
+	}
 }
